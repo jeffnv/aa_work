316013e3c5536534f230f25724a8f12851a8acbe
App Academy Student <student@appacademy.io>
2013-09-18 10:27:53 -0700

    add check guess method to codemaker class

--- w1/w1d3/mastermind.rb
+++ w1/w1d3/mastermind.rb
@@ -1,7 +1,6 @@
 class Game
   def initialize
     @board = Board.new
-    @player = Codebreaker.new
     @computer = Codemaker.new
   end
 
@@ -11,9 +10,18 @@ class Game
     end
   end
 
-  def play_turn
-    guess = @player.get_guess
+  def get_valid_guess
+    valid = false
+    while !valid do
+      #prompt
+      input_string
+      #check
+      #set valid
+    end
+  end
 
+  def play_turn
+    guess = get_valid_guess
     # pass guess to computer to get keycode
     # pass guess & keycode to board
 
@@ -27,36 +35,59 @@ end
 
 class Board
   def initialize
+    #do anything
   end
 
-    # board checks for all asterisks or full board
+  # board checks for all asterisks or full board
 end
 
 class  CodeMaker
   def initialize
-    @code = Code.new
+    @code
+  end
+
+  def is_guess_valid?(code)
+    true
   end
 
   def check_guess(guess)
+    code_copy = @code.dup
+    keycode = ""
+    code_copy.each_with_index do |code_char, i|
+      guess_char = guess[i]
+      if guess_char == code_char
+        keycode << "*"
+        code_copy[i] = guess[i] = nil
+      end
+    end
 
-    # returns keycode
+    code_copy.compact!
+    guess.compact!
+
+    guess.each_with_index do |guess_char, i|
+      if code_copy.include?(guess_char)
+        keycode << "."
+        code_copy[i] = guess[i] = nil
+      end
+    end
+    keycode
   end
 end
 
-class CodeBreaker
-  def initialize
-  end
 
-  def get_guess
-    puts "Enter guess:"
-    g
-
-  end
-end
-
-class KeyCode
-end
 
 class Code
+  VALID_PEGS = %w(R G B Y O P)
+  def initialize(input_code = nil)
+    @pegs = input_code || get_random_code
+
+  end
+
+  def get_random_code
+    rand_code = []
+    4.times do
+      rand_code << VALID_PEGS.sample
+    end
+  end
 
 end
