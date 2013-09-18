5d03c7916e0d076e2902812dfe9adbb6af132da1
App Academy Student <student@appacademy.io>
2013-09-18 13:23:59 -0700

    initial hangman commit

---
+++ w1/w1d3/hangman.rb
@@ -0,0 +1,30 @@
+class Hangman
+  def initialize
+  end
+
+  def play
+  end
+end
+
+class HumanPlayer
+  def initialize
+  end
+
+  def guess_letter
+  end
+
+  def confirm_guess
+  end
+
+end
+
+class ComputerPlayer
+  def initialize
+  end
+
+  def guess_letter
+  end
+
+  def confirm_guess
+  end
+end
