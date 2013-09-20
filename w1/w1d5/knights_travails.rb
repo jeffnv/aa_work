fc898562355083133d71f8bfeba3fd4d6a37bef1
jeffnv <student@appacademy.io>
2013-09-20 16:38:44 -0700

    refactor knight path

--- w1/w1d5/knights_travails.rb
+++ w1/w1d5/knights_travails.rb
@@ -3,12 +3,11 @@ load "tree_multiple_children.rb"
 
 class KnightPathFinder
   ADD_TO_KNIGHT_POS = [[2,1],[1,2],[-1,2],[-2,1],[-2,-1],[-1,-2],[1,-2],[2,-1]]
-  attr_reader :move_tree, :move_list
+  attr_reader :move_tree
 
   def initialize(start_pos)
     @start_pos = start_pos
     @move_tree = nil
-    @move_list = []
     build_move_tree
   end
 
@@ -18,19 +17,31 @@ class KnightPathFinder
 
     return [target_node.value] if target_node.parent.nil?
 
-    #search @move_tree for target_pos using dfs/bfs
-
     find_path(target_node.parent.value) + path
 
   end
 
   def trim_illegal_moves!(possible_moves)
+
     possible_moves.select! do |possible_move|
       possible_move.none?{|cooridinate| cooridinate < 0 || cooridinate > 7}
     end
+
   end
 
-  def search_possible_moves(current_position)
+  def trim_already_visited!(possible_moves)
+    possible_moves.each_with_index do |location_pair, index|
+
+      if @move_tree.bfs(location_pair)
+        possible_moves[index] = nil
+      end
+
+    end
+    possible_moves.compact!
+  end
+
+  def add_new_moves_to_tree(current_position)
+
     leaf_node = @move_tree.bfs(current_position)
 
     new_spaces = ADD_TO_KNIGHT_POS.map do |location_pair|
@@ -38,48 +49,23 @@ class KnightPathFinder
     end
 
     trim_illegal_moves!(new_spaces)
-
-
-    new_spaces.each_with_index do |location_pair, index|
-      if @move_tree.bfs(location_pair)
-        new_spaces[index] = nil
-      else
-        @move_list << location_pair
-        leaf_node.add_node(TreeNode.new(location_pair))
-      end
-    end
-
-    new_spaces.compact!
+    trim_already_visited!(new_spaces)
 
     new_spaces.each do |location_pair|
-        search_possible_moves(location_pair)
+      leaf_node.add_node(TreeNode.new(location_pair))
     end
 
+    new_spaces.each do |location_pair|
+      add_new_moves_to_tree(location_pair)
+    end
   end
 
   def build_move_tree
     @move_tree = TreeNode.new(@start_pos)
-    search_possible_moves(@start_pos)
-
-    #store in instance variable
-    #build tree using new_move_positions
-  end
-
-  def new_move_positions(pos)
-    #find positions you can move to from current position, up to 8 possible moves
-  end
-
-end
-
-class TreeNode
-  def path
-    #call parents to trace back route
-  end
-
-  def display_all_nodes
-
+    add_new_moves_to_tree(@start_pos)
   end
 end
+
 
 game = KnightPathFinder.new([0,0])
 p game.find_path([3,3])
