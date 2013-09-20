6ebe79230126e0213d4802856df7df7a2f27c259
App Academy Student <student@appacademy.io>
2013-09-19 19:00:34 -0700

    word chains working

--- w1/w1d4/word_chains.rb
+++ w1/w1d4/word_chains.rb
@@ -1,3 +1,4 @@
+require 'set'
 class WordChains
 
   def initialize
@@ -27,32 +28,55 @@ class WordChains
     @dictionary.select!{|word|word.length == word_to_compare.length}
   end
 
-  def find_chain(start_word, end_word)
+  def build_chain
+    chain = [@end_word]
+    chain_complete = false
+    current_word = @end_word
 
-    @visited_words = [start_word]
+    until chain_complete do
+      word_is_from = @visited_words[current_word]
+      chain << word_is_from
+      current_word = word_is_from
+      chain_complete = true if current_word == @start_word
+    end
+
+    p chain.reverse
+  end
+
+  def find_chain(start_word, end_word)
+    beginning_time = Time.now
+
+    @start_word = start_word
+    @visited_words = {start_word => nil}
     @current_words << start_word
     @end_word = end_word
     found = false
     until found do
       new_words = []
 
-      @current_words.each do |word|
+      @current_words.each_with_index do |word, index|
         adj_words =  adjacent_words(word)
-        new_words = new_words + (adj_words.select{|adj_word|!@visited_words.include?(adj_word)})
+        adj_words_not_yet_visited = (adj_words.select{|adj_word|!@visited_words.include?(adj_word)})
+        new_words = new_words + adj_words_not_yet_visited
+        adj_words_not_yet_visited.each do |adj_word_nyv|
+          @visited_words[adj_word_nyv] = word
+        end
       end
 
       if(new_words.include?(@end_word))
         puts "found word chain!"
+
         found = true
-        puts "#{@current_words.sort}"
+
       else
-        @visited_words += new_words
         @current_words = new_words
-        puts "#{@current_words[0..30]}"
+
       end
 
     end
-
+    build_chain
+    end_time = Time.now
+    puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
   end
 end
 
