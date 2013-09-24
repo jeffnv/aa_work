d970df76f3069f3a3138eac00fee9bdb60b73744
jeffnv <jeffnv@gmail.com>
2013-09-24 14:29:36 -0700

    add board generation, display; valid moves

--- w2/w2d2/chess/chess.rb
+++ w2/w2d2/chess/chess.rb
@@ -3,28 +3,92 @@ class Chess
 end
 
 class Board
+  PIECE_STARTING_LOCATIONS =
   def initialize
-    #build the board Array(8) { Array(8) {" "}}
-
+    generate_board
   end
 
-  #hits piece
+  def generate_board
+    @board = Array.new(8) do
+      Array.new(8) do
+        nil
+      end
+    end
+
+    init_color(:black)
+    init_color(:white)
+    @board
+  end
+
+  def init_color(color)
+
+    if(color == :black)
+      royal_row_idx = 0
+      pawn_row_idx = 1
+    else
+      royal_row_idx = 7
+      pawn_row_idx = 6
+    end
+
+    8.times do |col_index|
+      location = [royal_row_idx, col_index]
+      case col_index
+      when 0, 7
+        @board[royal_row_idx][col_index] = Rook.new(location, color, self)
+      when 1, 6
+        @board[royal_row_idx][col_index] = Knight.new(location, color, self)
+      when 2, 5
+        @board[royal_row_idx][col_index] = Bishop.new(location, color, self)
+      when 3
+        @board[royal_row_idx][col_index] = King.new(location, color, self)
+      when 4
+        @board[royal_row_idx][col_index] = Queen.new(location, color, self)
+      end
+
+      @board[pawn_row_idx][col_index] = Pawn.new(location, color, self)
+    end
+  end
+
+  def display
+    display_string = ""
+    @board.each_index do |row_idx|
+
+      @board[row_idx].each do |chess_piece|
+        if chess_piece
+          display_string << chess_piece.mark + " "
+        else
+          display_string << '  '
+        end
+      end
+      display_string << "\n"
+    end
+    puts display_string
+  end
+
+  def piece_at(location)
+    @board[location[0]][location[1]]
+  end
 
 
 end
 
 class Piece
-  attr_reader :marker, :color, :location
-  def initialize(location, board, color)
+  attr_reader :color, :location
+  def initialize(location, color, board)
     @location = location
-    @board = board
     @color = color
+    @board = board
   end
   #color
 
   #def move(coords)
   # => throws exception if coords is not in valid_moves?
   #end
+  def valid_moves
+    moves = get_moves
+  end
+
+
 
 
   private
@@ -45,20 +109,21 @@ class Piece
 end
 
 module SlidingPiece
-  def generate_slides
+  def get_moves
     moves = []
 
     MOVE_DIRS.each do |dir|
       temp_location = @location.dup
       slide_stopper_encountered = false
+
       while on_board?(temp_location) do
         temp_location = [temp_location[0] + dir[0], temp_location[1] + dir[1]]
 
-        break if @board.hits_friendly?
+        break if hits_friendly?
 
         moves << temp_location
 
-        break if @board.hits_enemy?
+        break if hits_enemy?
       end
     end
     moves
@@ -68,8 +133,14 @@ module SlidingPiece
 end
 
 module SteppingPiece
-  def generate_steps
-
+  def get_moves
+    moves = []
+    MOVE_OFFSETS.each do |row,col|
+      new_move = [row + @location[0], col + @location[1]]
+      next if  hits_friendly?(new_move) || !on_board?
+      moves << new_move
+    end
+    moves
   end
 
 end
@@ -77,43 +148,83 @@ end
 class Pawn < Piece
   MOVE_OFFSETS = []
 
+  def mark
+    if @color == :white
+      "\u2659"
+    else
+      "\u265F"
+    end
+  end
+
 end
 
 class Rook < Piece
   include SlidingPiece
   MOVE_DIRS = [[-1,0],[0,-1],[1,0],[0,1]]
 
-  def valid_moves
-    moves = generate_slides
-    moves = moves.reject{|move| @board.causes_self_check(@location, move)}
-    #filter moves beyond friendly piece
-
-    #filter moves that are BEYOND enemy square
-    #filter moves that would place piece in check
+  def mark
+    if @color == :white
+      "\u2656"
+    else
+      "\u265C"
+    end
   end
 
 end
 
 class Knight < Piece
 include SteppingPiece
-  MOVE_OFFSETS = []
+  MOVE_OFFSETS = [[-2, -1], [-1, -2], [-2, 1], [1, -2], [2, -1], [-1, 2], [2, 1], [1, 2]]
+  def mark
+    if @color == :white
+      "\u2658"
+    else
+      "\u265E"
+    end
+  end
+
 end
 
 class Bishop < Piece
 include SlidingPiece
-  MOVE_DIRS = [[-1,-1],[-1,1],[1,1],[1,-1]]
+  MOVE_DIRS = [[-1,-1],[-1,1],[1,1],[1,-1],]
 
-  #call generate_slides again. Maybe have Piece class call this
-
+  def mark
+    if @color == :white
+      "\u2657"
+    else
+      "\u265D"
+    end
+  end
 
 end
 
 class Queen < Piece
 include SlidingPiece
-  MOVE_DIRS = []
+  MOVE_DIRS = [[-1,-1],[-1,1],[1,1],[1,-1],[-1,0],[0,-1],[1,0],[0,1]]
+  def mark
+    if @color == :white
+      "\u2655"
+    else
+      "\u265B"
+    end
+  end
+
 end
 
 class King < Piece
 include SteppingPiece
-  MOVE_OFFSETS = []
+  MOVE_OFFSETS = [[-1,-1],[-1,1],[1,1],[1,-1],[-1,0],[0,-1],[1,0],[0,1]]
+  def mark
+    if @color == :white
+      "\u2654"
+    else
+      "\u265A"
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  chessboard = Board.new
+  chessboard.display
 end
