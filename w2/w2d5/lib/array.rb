96cfd985694483fd8181826ec0016e0cb911f0e8
App Academy Student <student@appacademy.io>
2013-09-27 13:39:37 -0700

    Finish array exercises

--- w2/w2d5/lib/array.rb
+++ w2/w2d5/lib/array.rb
@@ -6,4 +6,14 @@ class Array
       end
     end
   end
+
+  def two_sum
+    two_sums = []
+    self.each_index do |start_idx|
+      ((start_idx + 1)...self.length).each do |end_idx|
+        two_sums << [start_idx, end_idx] if (self[start_idx] + self[end_idx]).zero?
+      end
+    end
+    two_sums
+  end
 end
--- w2/w2d5/spec/array_spec.rb
+++ w2/w2d5/spec/array_spec.rb
@@ -1,9 +1,20 @@
 require 'rspec'
 require 'array'
 
-describe "#uniq" do
-  #Array has a uniq method that removes duplicates from an array. It returns the unique elements in the order in which they first appeared
-  it "returns the unique elements from the array" do
-    [1, 2, 1, 3, 3].my_uniq.should == [1,2,3]
+describe "Array" do
+  describe "#uniq" do
+    #Array has a uniq method that removes duplicates from an array. It returns the unique elements in the order in which they first appeared
+    it "returns the unique elements from the array" do
+      [1, 2, 1, 3, 3].my_uniq.should == [1,2,3]
+    end
+  end
+
+  describe "#two_sum" do
+    it "finds all pairs of positions where elements sum to zero" do
+      [-1, 0, 2, -2, 1].two_sum.should == [[0, 4], [2, 3]]
+    end
   end
 end
+
+
+
