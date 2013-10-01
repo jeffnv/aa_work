aae02df3d8da937b018c7e6813e076d8c659d7b4
= <=>
2013-10-01 10:53:34 -0700

    add questions.db, questions.rb

---
+++ w3/w3d2/questions.rb
@@ -0,0 +1,16 @@
+require 'singleton'
+require 'sqlite3'
+
+class QuestionsDatabase < SQLite3::Database
+
+  include Singleton
+
+  def initialize
+    super("questions.db")
+
+    self.results_as_hash = true
+
+    self.type_translation = true
+  end
+end
+
