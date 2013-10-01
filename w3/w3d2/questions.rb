7d59b591ebec2435654ea3b1a446730a8db993c7
= <=>
2013-10-01 11:14:52 -0700

    add find by and users.all

--- w3/w3d2/questions.rb
+++ w3/w3d2/questions.rb
@@ -14,3 +14,37 @@ class QuestionsDatabase < SQLite3::Database
   end
 end
 
+class User
+  attr_accessor :fname, :lname
+  def self.all
+    results = QuestionsDatabase.instance.execute("SELECT * FROM users")
+    results.map { |result| User.new(result) }
+  end
+
+  def self.find_by_id(id)
+    user_hash = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM
+        users
+      WHERE
+        id=?;
+    SQL
+
+    if user_hash.empty?
+      return nil
+    else
+      User.new(user_hash[0])
+    end
+  end
+
+  def initialize(options = {})
+    @id = options["id"]
+    @fname = options["fname"]
+    @lname = options["lname"]
+  end
+
+  def to_s
+    "#{@fname} #{@lname}"
+  end
+
+end
