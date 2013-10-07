dd6c198e6b013df04bd70b44daf80212311d762d
Jeffrey Fiddler <jeffnv@gmail.com>
2013-10-07 10:39:43 -0700

    first phase of active record lite complete'

---
+++ w4/active_record_lite/lib/active_record_lite/mass_object.rb
@@ -0,0 +1,29 @@
+class MassObject
+  def self.my_attr_accessible(*attributes)
+    @attributes = attributes
+    attributes.each do |sym|
+      attr_accessor sym
+    end
+  end
+
+  def self.attributes
+    @attributes
+  end
+
+  def self.parse_all(results)
+  end
+
+  def initialize(params = {})
+    params.each do |attr_name, val|
+      if(self.class.attributes.include?(attr_name))
+        instance_variable_set('@' << attr_name.to_s, val)
+      else
+        raise "mass assignment to unregistered attribute #{attr_name}"
+      end
+    end
+  end
+end
+
+class Cat < MassObject
+  my_attr_accessible :sound
+end
