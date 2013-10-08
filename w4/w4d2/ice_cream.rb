78aeac808c27c57f0866422783cf977122df38bf
App Academy Student <student@appacademy.io>
2013-10-08 14:47:59 -0700

    refactor ice cream

--- w4/w4d2/ice_cream.rb
+++ w4/w4d2/ice_cream.rb
@@ -3,55 +3,61 @@ require 'json'
 require 'addressable/uri'
 require 'nokogiri'
 
-address = "1061 Market St., San Francisco, CA 94103"
-key = 'AIzaSyASex8XVQrc__dIeS_J7lmhxeZ33LYt4qI'
+ADDRESS = "1061 Market St., San Francisco, CA 94103"
+KEY = 'AIzaSyASex8XVQrc__dIeS_J7lmhxeZ33LYt4qI'
 LOCATION_COUNT = 3
 
+def get_my_coords
+  query = Addressable::URI.new(
+     :scheme => "https",
+     :host => "maps.googleapis.com",
+     :path => "/maps/api/geocode/json",
+     :query_values => { :address => ADDRESS,
+                        :sensor => false
+                      }
+   ).to_s
 
-query = Addressable::URI.new(
-   :scheme => "https",
-   :host => "maps.googleapis.com",
-   :path => "/maps/api/geocode/json",
-   :query_values => { :address => address,
-                      :sensor => false
-                    }
- ).to_s
+  place_hash = JSON.parse(RestClient.get(query))
 
-#
+  results = place_hash["results"].first
 
-place_hash = JSON.parse(RestClient.get(query))
+  my_loc = results['geometry']['location']
+  my_loc = "#{my_loc['lat']},#{my_loc['lng']}"
 
-results = place_hash["results"].first
-
-my_loc = results['geometry']['location']
-my_loc = "#{my_loc['lat']},#{my_loc['lng']}"
-# https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters
-
-
-#======GET PLACES
-query = Addressable::URI.new(
-   :scheme => "https",
-   :host => "maps.googleapis.com",
-   :path => "/maps/api/place/nearbysearch/json",
-   :query_values => { :sensor => false,
-                      key: key,
-                      location: my_loc,
-                      radius: 1000,
-                      keyword: 'ice cream',
-                      type: 'food'
-
-                    }
- ).to_s
-
-ice_cream_places  = JSON.parse(RestClient.get(query))['results'][0...LOCATION_COUNT]
-
-destinations =[]
-ice_cream_places.each do |place|
-  name = place['name']
-  ice_cream_loc = place["geometry"]["location"]
-  ice_cream_loc = "#{ice_cream_loc['lat']},#{ice_cream_loc['lng']}"
-  destinations << [name, ice_cream_loc]
 end
+
+
+
+def get_ice_cream_destinations(my_loc)
+  #======GET PLACES
+  query = Addressable::URI.new(
+     :scheme => "https",
+     :host => "maps.googleapis.com",
+     :path => "/maps/api/place/nearbysearch/json",
+     :query_values => { :sensor => false,
+                        key: KEY,
+                        location: my_loc,
+                        radius: 1000,
+                        keyword: 'ice cream',
+                        type: 'food'
+
+                      }
+   ).to_s
+
+  ice_cream_places  = JSON.parse(RestClient.get(query))['results'][0...LOCATION_COUNT]
+
+  destinations =[]
+  ice_cream_places.each do |place|
+    name = place['name']
+    ice_cream_loc = place["geometry"]["location"]
+    ice_cream_loc = "#{ice_cream_loc['lat']},#{ice_cream_loc['lng']}"
+    destinations << [name, ice_cream_loc]
+  end
+  destinations
+end
+
+my_loc = get_my_coords
+destinations = get_ice_cream_destinations(my_loc)
 
 #=========GET DIRECTIONS
 destinations.each do |destination|
