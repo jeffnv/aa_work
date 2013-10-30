a86d551b8aacae98749f4b2b9765ad1e3512d874
Jeffrey Fiddler <jeffnv@gmail.com>
2013-10-30 14:28:21 -0700

    inline edit shows up but has no effect

--- w7/d2/Journal/app/assets/javascripts/views/post_show.js
+++ w7/d2/Journal/app/assets/javascripts/views/post_show.js
@@ -1,11 +1,24 @@
 Journal.Views.PostShow = Backbone.View.extend({
+  template: JST['posts/post_show'],
+  events: {
+  'dblclick .post-title': 'handleDblClick',
+  'dblclick .post-body': 'handleDblClick',
+  },
+  
+  handleDblClick: function(event){
+    var $item = $(event.currentTarget);
+    var class_name = $item.attr('class');
+    var splitIdx = class_name.indexOf("shown") - 1;
+    class_name =  "." + class_name.slice(0, splitIdx);
+    
+    $(class_name).toggleClass('shown not-shown')
+    //get the item clicked
+    //toggle its class to not-shown
+  },
+  
   render: function(){
-    this.$el.html("<h3>" + this.model.get('title') + "</h3>");
-    this.$el.append("<p>" + this.model.get('body') + "</p>");
-    this.$el.append("<a href=#>All Posts</a>")
-    this.$el.append(
-      '<br><a href="#/posts/'+ this.model.id +'/edit' +'">Edit</a>'
-    )
+    var renderedContent = this.template({post: this.model});
+    this.$el.html(renderedContent);
     return this;
   }
 });
