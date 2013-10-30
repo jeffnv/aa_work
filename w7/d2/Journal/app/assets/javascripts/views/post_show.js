2548f7bec6b971680ab108a13aa8ab28798d128b
Jeffrey Fiddler <jeffnv@gmail.com>
2013-10-30 11:45:21 -0700

    Added lovely bootstrap

--- w7/d2/Journal/app/assets/javascripts/views/post_show.js
+++ w7/d2/Journal/app/assets/javascripts/views/post_show.js
@@ -3,7 +3,9 @@ Journal.Views.PostShow = Backbone.View.extend({
     this.$el.html("<h3>" + this.model.get('title') + "</h3>");
     this.$el.append("<p>" + this.model.get('body') + "</p>");
     this.$el.append("<a href=#>All Posts</a>")
-    this.$el.append('<br><a href="#/posts/'+ this.model.id +'/edit' +'">Edit</a>')
+    this.$el.append(
+      '<br><a href="#/posts/'+ this.model.id +'/edit' +'">Edit</a>'
+    )
     return this;
   }
 });
