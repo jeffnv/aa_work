8ca51c4edc023c2fe865f4e9a26cabd51023814f
Jeffrey Fiddler <jeffnv@gmail.com>
2013-10-29 19:07:57 -0700

    edit for works

--- w7/d2/Journal/app/assets/javascripts/routers/post_router.js
+++ w7/d2/Journal/app/assets/javascripts/routers/post_router.js
@@ -6,17 +6,23 @@ Journal.PostRouter = Backbone.Router.extend({
   },
   
   showAllPosts: function(){
-    var postIndexView = new Journal.Views.PostsIndex({collection:Journal.posts});
+    var postIndexView = new Journal.Views.PostsIndex({
+      collection:Journal.posts
+    });
     $('div').html(postIndexView.render().$el);
   },
   
   showPost: function(id){
-    var postShowView = new Journal.Views.PostShow({model: Journal.posts.get(id)});
+    var postShowView = new Journal.Views.PostShow({
+      model: Journal.posts.get(id)
+    });
     $('div').html(postShowView.render().$el);
   },
   
   showEditForm: function(id){
-    var postEditView = new Journal.Views.PostForm({model: Journal.posts.get(id)});
+    var postEditView = new Journal.Views.PostForm({
+      model: Journal.posts.get(id)
+    });
     $('div').html(postEditView.render().$el);
   }
 });
--- w7/d2/Journal/app/assets/javascripts/views/post_form.js
+++ w7/d2/Journal/app/assets/javascripts/views/post_form.js
@@ -1,7 +1,9 @@
 Journal.Views.PostForm = Backbone.View.extend({
-  template: JST['posts/post_form'],
+  template: JST["posts/post_form"],
   render: function(){
-    var rC = this.template();
+    var rC = this.template({
+      post: this.model
+    });
     this.$el.html(rC);
     return this
   }
