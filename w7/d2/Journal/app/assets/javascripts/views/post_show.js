0e7b340f389d807781f708d7905baae4a9b080fd
Jeffrey Fiddler <jeffnv@gmail.com>
2013-10-30 17:40:38 -0700

    finish all parts of exercise

--- w7/d2/Journal/app/assets/javascripts/views/post_show.js
+++ w7/d2/Journal/app/assets/javascripts/views/post_show.js
@@ -5,28 +5,34 @@ Journal.Views.PostShow = Backbone.View.extend({
   
   template: JST['posts/post_show'],
   events: {
-  'dblclick .post-title': 'handleDblClick',
-  'dblclick .post-body': 'handleDblClick',
-  'submit form': 'handleSubmit'
+  'dblclick .post-title': 'switchVisibility',
+  'dblclick .post-body': 'switchVisibility',
+  'blur .post-title': 'saveEditToDB',
+  'blur .post-body': 'saveEditToDB',
+  'submit form': 'submitNullifyer'
   },
   
-  handleDblClick: function(event){
+  switchVisibility: function(event){
     var $item = $(event.currentTarget);
     var class_name = $item.attr('class');
     var splitIdx = class_name.indexOf("shown") - 1;
     class_name =  "." + class_name.slice(0, splitIdx);
-    
-    $(class_name).toggleClass('shown not-shown')
-    //get the item clicked
-    //toggle its class to not-shown
+    var $els = $(class_name);
+    $els.toggleClass('shown not-shown');
+    $('input'+class_name).focus();
   },
   
-  handleSubmit: function(event){
+  saveEditToDB: function(event){
     event.preventDefault();
     var payload = $(event.currentTarget).serializeJSON();
-    this.handleDblClick(event);
+    this.switchVisibility(event);
     this.model.set(payload.post); 
     this.model.save();  
+  },
+  
+  submitNullifyer: function(event){
+    event.preventDefault();
+    this.saveEditToDB(event);
   },
   
   render: function(){
